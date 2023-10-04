10a665c497f3985c5fa243709e0836966fb4632d
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-05 01:47:04 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnShow = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnShow
+        ' 
+        btnShow.BackColor = SystemColors.ControlDarkDark
+        btnShow.Location = New Point(327, 158)
+        btnShow.Name = "btnShow"
+        btnShow.Size = New Size(75, 23)
+        btnShow.TabIndex = 0
+        btnShow.Text = "Show"
+        btnShow.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = SystemColors.ControlLight
+        Label1.Location = New Point(226, 92)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(45, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "MyText"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(404, 84)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(166, 23)
+        TextBox1.TabIndex = 2
+        TextBox1.Text = "Visual Basic 2022"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDarkDark
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(btnShow)
+        ForeColor = SystemColors.ControlDark
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnShow As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim MyText As String
+        MyText = "Visual Basic 2022"
+        MsgBox(Len(MyText))
+    End Sub
+End Class
